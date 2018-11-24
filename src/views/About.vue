a61449e4ada329320ea29420549b9b65637768f1
Teo Wei Shen <tws4793@gmail.com>
2018-11-25 01:18:07 +0800

    Add more description to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,8 @@
 <template lang="pug">
-  span This allows you to locate your child
+  .content.has-text-justified
+    p The concept behind the solution is to give children who enter Downtown East a bracelet; this bracelet acts as a beacon that periodically pings its location. These pings are then picked up by receivers that are placed throughout Downtown East. The beacon’s ID is also paired to an app in the parent’s smartphone.
+    p All the receivers will be wirelessly connected to the same network; this network will then allow parents/mall management to track a beacon that is worn by the child. By knowing which receiver has last received the beacon signal, they will be able to identify the location that the child is currently at. This would be helpful as:
+    ul
+      li Parents can check as and when they want to locate their child
+      li Parents can be notified if some event happens, for example a child goes missing, or wander off
 </template>
