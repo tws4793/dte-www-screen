68bd53e1e46527df435e19d93a5c4071a240c5a7
Teo Wei Shen <tws4793@gmail.com>
2018-11-25 15:36:38 +0800

    Improve map overlay

--- src/views/Locator.vue
+++ src/views/Locator.vue
@@ -1,19 +1,50 @@
 <template lang="pug">
-  Map
+  .columns
+    .column
+    .column.is-two-thirds
+      h2 Phone Number: #[strong {{ lastSeen.phoneNo }}]
+      h2 Your child was last seen at #[strong {{ lastSeen.time }}]
+      #map
+        img.img(src='../assets/smu-labs-01.jpg')
+        .overlay(:style='overlayArea')
+      router-link.button.is-success.is-large(to='/') Back
+    .column
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-import Map from "@/components/Map.vue";
-import Prompt from "@/components/Prompt.vue";
+import { Component, Prop, Provide, Vue } from "vue-property-decorator";
 
-@Component({
-  components: {
-    HelloWorld,
-    Prompt,
-    Map
+@Component
+export default class Locator extends Vue {
+  zones = {
+    1: {right: 0, top: 0},
+    2: {right: 0, bottom: '7px'},
+    3: {left: 0, bottom: '7px'},
+    4: {left: 0, top: 0},
   }
-})
-export default class Locator extends Vue {}
+
+  mounted() {
+    const qInterval = setInterval(this.$store.dispatch('queryTracker', this.lastSeen.phoneNo), 5000)
+  }
+  get lastSeen() {
+    return this.$store.state.lastSeen
+  }
+  get overlayArea() {
+    return this.zones[this.lastSeen.zone]
+  }
+}
 </script>
+
+<style lang='scss'>
+#map {
+  position: relative;
+}
+.overlay {
+  position: absolute;
+  background: rgba(255, 0, 0, 0.5); /* Red see-through */
+  transition: 0.5s ease;
+  opacity: 1;
+  width: 50%;
+  height: 50%;
+}
+</style>
